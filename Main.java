b3a6f1008f929ec33abb9748dca78322a23a881b
Anastasiia Zibla <111114242+mirage109@users.noreply.github.com>
2022-09-19 23:11:39 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+//Написать метод, возвращающий целое число равное двойной длине строки, принимаемой в качестве аргумента метода.  Пример: hello -> 10
+        String text="Hello World";
+        System.out.println(text);
+        System.out.println(doubleLength(text));
+    }
+    public static int doubleLength(String s){
+        return s.length()*2;
+    }
+}
